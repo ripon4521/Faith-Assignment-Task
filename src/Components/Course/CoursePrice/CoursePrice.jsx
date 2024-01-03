31556cf08325c8952549df18be8a05345934b97c
ripon4521 <riponakondo4521@gmail.com>
2024-01-03 22:16:25 +0600

    cretae course price secion

--- src/Components/Course/CoursePrice/CoursePrice.jsx
+++ src/Components/Course/CoursePrice/CoursePrice.jsx
@@ -2,22 +2,22 @@
 
 const CoursePrice = () => {
     return (
-        <div>
+        <div className="">
    <div className="mx-auto max-w-3xl  px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
   <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-center md:gap-8">
     <div
-      className="rounded-2xl bg-red-100 border border-indigo-600 p-6 shadow-sm ring-1 ring-indigo-600 sm:order-last sm:px-8 lg:p-12"
+      className="rounded-2xl bg-slate-200 border border-indigo-600 p-6 shadow-sm ring-1 ring-indigo-600 sm:order-last sm:px-8 lg:p-12"
     >
       <div className="text-center">
-        <h2 className="text-lg font-medium text-gray-900">
-          Pro
-          <span className="sr-only">Plan</span>
+        <h2 className="text-2xl font-medium text-gray-900">
+          Course Fee
+     
         </h2>
 
         <p className="mt-2 sm:mt-4">
-          <strong className="text-3xl font-bold text-gray-900 sm:text-4xl"> 30$ </strong>
+          <strong className="text-3xl font-bold text-gray-900 sm:text-4xl"> $5000 </strong>
 
-          <span className="text-sm font-medium text-gray-700">/month</span>
+        
         </p>
       </div>
 
@@ -34,7 +34,7 @@ const CoursePrice = () => {
             <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
           </svg>
 
-          <span className="text-gray-700"> 20 users included </span>
+          <span className="text-gray-700"> 5 videos </span>
         </li>
 
         <li className="flex items-center gap-1">
@@ -49,7 +49,7 @@ const CoursePrice = () => {
             <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
           </svg>
 
-          <span className="text-gray-700"> 5GB of storage </span>
+          <span className="text-gray-700"> 2 Live Session </span>
         </li>
 
         <li className="flex items-center gap-1">
@@ -64,7 +64,7 @@ const CoursePrice = () => {
             <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
           </svg>
 
-          <span className="text-gray-700"> Email support </span>
+          <span className="text-gray-700">Live Q&NA Session  </span>
         </li>
 
         <li className="flex items-center gap-1">
@@ -79,38 +79,11 @@ const CoursePrice = () => {
             <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
           </svg>
 
-          <span className="text-gray-700"> Help center access </span>
+          <span className="text-gray-700"> Whatsapp Coummnity </span>
         </li>
 
-        <li className="flex items-center gap-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="h-5 w-5 text-indigo-700"
-          >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-          </svg>
+     
 
-          <span className="text-gray-700"> Phone support </span>
-        </li>
-
-        <li className="flex items-center gap-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="h-5 w-5 text-indigo-700"
-          >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-          </svg>
-
-          <span className="text-gray-700"> Community access </span>
-        </li>
       </ul>
 
       <a
